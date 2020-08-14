32ae18117c22cb476f01da3b8fbfb23d382e2184
SMSLink.ro <contact@smslink.ro>
2020-08-14 23:39:07 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,2 @@
+curl --request GET \
+	--url 'https://secure.smslink.ro/sms/gateway/communicate/index.php?connection_id=MyConnectionID&password=MyConnectionPassword&to=07xyzzzzzz&message=TestMessage'
